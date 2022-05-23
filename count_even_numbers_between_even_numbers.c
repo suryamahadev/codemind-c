69f56cf025e96661c37f55c5aea19c02f8a10a5a
mahadev <97836240+suryamahadev@users.noreply.github.com>
2022-05-23 10:32:00 +0530

    #include<stdio.h> int main(){ int arr[100],n,i,r; scanf("%d",&n); for(i=0;i<n;i++) scanf("%d",&arr[i]); for(i=1;i<n-1;i++) { if(arr[i]%2==0) { if(arr[i-1]%2==0 && arr[i+1]%2==0) { r++; } } } printf("%d",r); }

---
+++ count_even_numbers_between_even_numbers.c
@@ -0,0 +1,2 @@
+#include<stdio.h>
+int main(){ int arr[100],n,i,r; scanf("%d",&n); for(i=0;i<n;i++) scanf("%d",&arr[i]); for(i=1;i<n-1;i++) { if(arr[i]%2==0) { if(arr[i-1]%2==0 && arr[i+1]%2==0) { r++; } } } printf("%d",r); }
