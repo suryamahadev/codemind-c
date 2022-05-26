e621c0951d8dbea577793222a6e5c4178d962ab3
mahadev <97836240+suryamahadev@users.noreply.github.com>
2022-05-26 18:49:08 +0530

    #include<stdio.h> int main() {     int n,d,s=0;     scanf("%d",&n);     while(n>=10)     {         s=0;         while(n)         {             d=n%10;             s+=d;             n=n/10;             }             n=s;             }             printf("%d",n); }

---
+++ Add_Digits.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{ 
+    int n,d,s=0;
+    scanf("%d",&n);
+    while(n>=10)
+    { 
+        s=0; 
+        while(n)
+        { 
+            d=n%10;
+            s+=d;
+            n=n/10;
+            } 
+            n=s;
+            } 
+            printf("%d",n);
+}
