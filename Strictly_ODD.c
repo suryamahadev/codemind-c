276079b83145267f96cb5c5849f59035b4e531ca
mahadev <97836240+suryamahadev@users.noreply.github.com>
2022-06-01 18:05:56 +0530

    #include<stdio.h> int main() {     int arr[100],n,c=0,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         }         for(i=0;i<n;i++)         {             if(arr[i]%2!=0)             {                 if(i%2==0)                 {                     printf("False ");                     return 0;
                    }
                    }
                    }
                    printf("True
    ");
                    return 0;
        
    }

---
+++ Strictly_ODD.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,c=0,i; 
+    scanf("%d",&n);
+    for(i=0;i<n;i++) 
+    {
+        scanf("%d",&arr[i]);
+        }
+        for(i=0;i<n;i++) 
+        {
+            if(arr[i]%2!=0)
+            {
+                if(i%2==0)
+                {
+                    printf("False
+");
+                    return 0; 
+                    
+                }
+                }
+                }
+                printf("True
+");
+                return 0;
+    
+}
