af919afefa8dcc16582ba9f796840bc564223c76
mahadev <97836240+suryamahadev@users.noreply.github.com>
2022-06-02 18:47:55 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,k=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         // printf("%d ",arr[i]);         }         for(i=0;i<n;i++)         { if(arr[i]%2==0) { k++; } } if(k==n) { printf("True"); } else { printf("False"); } }

--- Even_Array.c
+++ Even_Array.c
@@ -1,27 +1,12 @@
 #include<stdio.h>
 int main()
-{ 
+{
     int n,arr[100],i,k=0;
-    scanf("%d",&n); 
-    for(i=0;i<n;i++)
+    scanf("%d",&n);
+    for(i=0;i<n;i++) 
     {
         scanf("%d",&arr[i]);
-        // printf("%d ",arr[i]);
+        // printf("%d ",arr[i]); 
         }
         for(i=0;i<n;i++)
-        {
-            if(arr[i]%2==0) 
-            {
-                k++;
-                }
-                }
-                if(k==n)
-                {
-                    printf("True");
-                    }
-                    else
-                    {
-                        printf("False");
-                        }
-    
-}
+        { if(arr[i]%2==0) { k++; } } if(k==n) { printf("True"); } else { printf("False"); } }
